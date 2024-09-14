8e28167e785c9eeaf0f1d3011e45c33ad2461e84
Zak <zakariabouaddou@gmail.com>
2024-09-14 14:04:06 -0700

    Add project files.

---
+++ Activity6Program.cpp
@@ -0,0 +1,31 @@
+// program ask for user's info and displays outcome
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // declare variable to store user input
+    string city;
+    string zipcode;
+    string street;
+    string state;
+    int zipcode;
+
+    cout << "What street are you from? " << endl;
+    cin >> street;
+
+    cout << "What city are you from? " << endl;
+    cin >> city;
+
+    cout << "What state are you from? " << endl;
+    cin >> state;
+
+    cout << "What is you zipcode? " << endl;
+    cin >> zipcode;
+
+    cout << "Street: " << street << endl;
+    cout << "City, State, Zipcode: " << city << "," << state << zipcode;
+
+}
+
