c335d9465bf44879cd926c3c308a52dd0b3862e1
Zak <zakariabouaddou@gmail.com>
2024-11-11 13:04:42 -0800

    modified code to include getline() and also put user input seperately for each category. Included detailed comments aswell.

--- Activity6Program.cpp
+++ Activity6Program.cpp
@@ -1,30 +1,38 @@
-// program ask for user's info and displays outcome
 #include <iostream>
+#include <string> // Include the string library to use the string class
 
 using namespace std;
 
 int main()
 {
-    // declare variable to store user input
+    // Declare variables to store user input
     string city;
     string street;
     string state;
     int zipcode;
 
+    // Ask for the user's street (use getline to allow spaces in the street name)
     cout << "What street are you from? " << endl;
-    cin >> street;
+    getline(cin, street);
 
+    // Ask for the user's city (again using getline)
     cout << "What city are you from? " << endl;
-    cin >> city;
+    getline(cin, city);
 
+    // Ask for the user's state (using getline)
     cout << "What state are you from? " << endl;
-    cin >> state;
+    getline(cin, state);
 
-    cout << "What is you zipcode? " << endl;
+    // Ask for the user's zipcode (using cin as zipcode is a single number)
+    cout << "What is your zipcode? " << endl;
     cin >> zipcode;
 
+    // Output the gathered information
     cout << "Street: " << street << endl;
-    cout << "City, State, Zipcode: " << city << ", " << state << ", " << zipcode;
+    cout << "City: " << city << endl;
+    cout << "State:  " << state << endl;
+    cout << "Zipcode: " << zipcode << endl;
 
+    return 0;
 }
 
